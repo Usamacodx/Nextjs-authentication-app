b513f5d56a000de9660c5cba8887046acb485637
Usamacodx <usamagog7@example.com>
2024-09-23 01:58:18 +0500

    Your message

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,23 +1,15 @@
-import { NextRequest } from "next/server";
-import jwt from  'jsonwebtoken';
+import { NextRequest } from 'next/server';
+import jwt from 'jsonwebtoken';
+
 interface DecodedToken {
-    id: string; 
-  }
-
-export const getDataFromToken=(request: NextRequest)=>{
-    try {
-        const token=request.cookies.get('token')?.value ||  '';
-        const decodedToken:any=jwt.verify(token,"3435345");
-        return decodedToken.id;
-    } catch (error) {
-        console.error(error);
-        
-    }
-   
-
-
-
-
+  id: string; 
 }
-
-
+export const getDataFromToken = (request: NextRequest): string | undefined => {
+  try {
+    const token = request.cookies.get('token')?.value || '';
+    const decodedToken = jwt.verify(token, "3435345") as DecodedToken; // Explicitly cast the type
+    return decodedToken.id;
+  } catch (error) {
+    console.error(error);
+  }
+};
